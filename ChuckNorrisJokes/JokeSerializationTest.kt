4e7d0d5ce3000b0a95181c75a9fabc690be4d643
Nicolas Duponchel <nduponchel@gopro.com>
2020-02-27 10:03:22 +0100

    Fix test file (sorry guys)

--- ChuckNorrisJokes/JokeSerializationTest.kt
+++ ChuckNorrisJokes/JokeSerializationTest.kt
@@ -11,19 +11,23 @@ import org.junit.Test
  */
 class JokeSerializationTest {
     private val baseJson =
-        """{"icon_url":"https://assets.chucknorris.host/img/avatar/chuck-norris.png","id":"kvbADxuyS36ug4MJ7KMBMA","url":"https://api.chucknorris.io/jokes/kvbADxuyS36ug4MJ7KMBMA","value":"Chuck Norris puts mustard gas on his hotdogs."}"""
+        """{"categories":[],"created_at":"2020-01-05 13:42:29.296379","icon_url":"https://assets.chucknorris.host/img/avatar/chuck-norris.png","id":"p3GHoau2SLGTv04XhtIeeg","updated_at":"2020-01-05 13:42:29.296379","url":"https://api.chucknorris.io/jokes/p3GHoau2SLGTv04XhtIeeg","value":"Which came first - the chicken or the egg? Chuck Norris said it was the beer which came first, the hell with these damn animals!!!"}"""
 
     private val baseJoke = Joke(
+        categories = emptyList(),
+        createdAt = "2020-01-05 13:42:29.296379",
         iconUrl = "https://assets.chucknorris.host/img/avatar/chuck-norris.png",
-        id = "kvbADxuyS36ug4MJ7KMBMA",
-        url = "https://api.chucknorris.io/jokes/kvbADxuyS36ug4MJ7KMBMA",
-        value = "Chuck Norris puts mustard gas on his hotdogs."
+        id = "p3GHoau2SLGTv04XhtIeeg",
+        updatedAt = "2020-01-05 13:42:29.296379",
+        url = "https://api.chucknorris.io/jokes/p3GHoau2SLGTv04XhtIeeg",
+        value = "Which came first - the chicken or the egg? Chuck Norris said it was the beer which came first, the hell with these damn animals!!!"
     )
 
     @Test
     fun `serialization is correct`() {
         val json = Json(JsonConfiguration.Stable).stringify(Joke.serializer(), baseJoke)
         Assert.assertEquals(baseJson, json)
+
     }
 
     @Test
