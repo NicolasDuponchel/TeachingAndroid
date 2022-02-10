217c59de1f72773d3bb80a9a688698829daaf3a8
Nicolas Duponchel <nduponchel@gopro.com>
2022-02-10 14:26:02 +0100

    MyFirstApp Instructions Q3/4/5 Btn + implem

--- MyFirstApp/MyFirstApp/app/src/main/java/com/ndup_esiee/myfirstapp/MainActivity.kt
+++ MyFirstApp/MyFirstApp/app/src/main/java/com/ndup_esiee/myfirstapp/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.ndup_esiee.myfirstapp
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
+
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btn_try_me.setOnClickListener { tv_title.text = "${tv_title.text}E" }
     }
 }
