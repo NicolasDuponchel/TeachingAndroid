17bc44873b8e027e402586d6341b94f9f594b7b5
Nicolas Duponchel <nduponchel@gopro.com>
2022-02-10 14:32:44 +0100

    MyFirstApp Instructions Q6 names list

--- MyFirstApp/MyFirstApp/app/src/main/java/com/ndup_esiee/myfirstapp/MainActivity.kt
+++ MyFirstApp/MyFirstApp/app/src/main/java/com/ndup_esiee/myfirstapp/MainActivity.kt
@@ -3,6 +3,7 @@ package com.ndup_esiee.myfirstapp
 import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import kotlinx.android.synthetic.main.activity_main.*
 
 
@@ -14,5 +15,13 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         btn_try_me.setOnClickListener { tv_title.text = "${tv_title.text}E" }
+
+        listOf("Nico", "Pierre", "Paul", "Jacques", "Alex")
+            .sortedBy { it.count() }
+            .let { names ->
+                Log.i("Question 6", "names=$names")
+                tv_names.text = names.toString()
+            }
+
     }
 }
