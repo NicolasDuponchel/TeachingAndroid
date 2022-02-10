b709e81b6a0fe588ea15817645b9db3b1ba619b1
Nicolas Duponchel <nduponchel@gopro.com>
2022-02-10 10:44:58 +0100

    MyFirstApp Instructions Q1

---
+++ MyFirstApp/MyFirstApp/app/src/main/java/com/ndup_esiee/myfirstapp/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.ndup_esiee.myfirstapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
